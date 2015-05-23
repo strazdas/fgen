657a2e907f26d215fd85ae34f19c7f63fe75b597
niekas <albertas.gimbutas@mii.vu.lt>
2015-05-23 15:37:25 +0300

    Add function_id argument to GKLS function evaluation script.

--- GKLS_C/main.cpp
+++ GKLS_C/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     // assert(GKLS_set_default()== GKLS_OK);     // Standartiniai nustatymai
-    if (argc < 7) {
+    if (argc < 8) {
         cout << "Not enought arguments." << endl;
-        cout << "  Specify: dimension, global_dist, global_radius, num_minima and a point where to evaluate function." << endl;
+        cout << "  Specify: dimension, global_dist, global_radius, num_minima, function_id and a point where to evaluate function." << endl;
         return 1;
     }
 
@@ -19,17 +19,18 @@ int main (int argc, char *argv[]) {
     GKLS_global_radius = atof(argv[3]);
     GKLS_num_minima = atoi(argv[4]);
     GKLS_global_value = GKLS_GLOBAL_MIN_VALUE;
+    int function_id = atoi(argv[5]);
     assert(GKLS_domain_alloc() == GKLS_OK);
 
     double point[GKLS_dim];
     for (unsigned int i = 0; i < GKLS_dim; i++) {
         GKLS_domain_left[i] = -1;
         GKLS_domain_right[i] = 1;
-        point[i] = atof(argv[i+5]);
+        point[i] = atof(argv[i+6]);
     }
 
     assert(GKLS_parameters_check() == GKLS_OK);
-    assert(GKLS_arg_generate(1) == GKLS_OK);
+    assert(GKLS_arg_generate(function_id) == GKLS_OK);
     cout << GKLS_D_func(point) << endl;
     GKLS_free();
     GKLS_domain_free();
